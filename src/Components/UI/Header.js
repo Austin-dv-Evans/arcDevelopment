76e2be707ddf170882ee20ca87047def44457a64
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-10-17 22:16:21 -0600

    navigation, router, tabs complete

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core';
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom'
 
 import logo from '../../assets/logo.svg'
-
 
 function ElevationScroll(props) {
     const { children} = props;
@@ -26,19 +29,98 @@ function ElevationScroll(props) {
           marginBottom: "3em"
       },
       logo :{
-          height: "7em"
-      }
+          height: "8em"
+      },
+      logoContainer: {
+        padding: 0,
+        "&:hover" : {
+            backgroundColor: "transparent"
+        }
+      },
+      tabContainer: {
+          marginLeft: "auto"
+      },
+      tab: {
+        ...theme.typography.tab,
+        minWidth: 10,
+        marginLeft: "25px"
+      },
+      button: {
+          ...theme.typography.estimate,
+          borderRadius: "50px",
+          marginLeft: "50px",
+          marginRight: "50px",
+          height: "45px",
+      },
+      
   }))
 
 export default function Header(props){
     const classes = useStyles()
+    const [value, setValue] = useState(0)
+    
+    const handleChange = (e, value) => {
+        setValue(value)
+    }
+
+    useEffect(() => {
+        if(window.location.pathname === "/" && value !== 0) {
+            setValue(0)
+        } else if (window.location.pathname === "/services" && value !== 1) {
+            setValue(1)
+        } else if (window.location.pathname === "/revolution" && value !== 2) {
+            setValue(2)
+        } else if (window.location.pathname === "/about" && value !== 3) {
+            setValue(3)
+        } else if (window.location.pathname === "/contact" && value !== 4) {
+            setValue(4)
+        } else if (window.location.pathname === "/estimate" && value !== 5) {
+            setValue(5)
+        }
+    }, [value])
 
     return(
         <>
             <ElevationScroll>
                 <AppBar position="fixed" color="primary">
                     <Toolbar disableGutters>
-                        <img alt="company logo" src={logo} className={classes.logo}/>
+                        <Button disableRipple component={Link} to="/" onClick={() =>
+                             setValue(0)} className={classes.logoContainer} > 
+                            <img alt="company logo" src={logo} className={classes.logo}/>
+                        </Button>
+                        <Tabs 
+                            value={value} 
+                            className={classes.tabContainer} 
+                            onChange={handleChange}
+                            indicatorColor="secondary">
+                            <Tab 
+                                label="Home" 
+                                className={classes.tab} 
+                                component={Link} to="/"/>
+                            <Tab 
+                                label="Services" 
+                                className={classes.tab} 
+                                component={Link} to="/services"/>
+                            <Tab 
+                                label="The Revolution" 
+                                className={classes.tab} 
+                                component={Link} to="/revolution"/>
+                            <Tab 
+                                label="About Us" 
+                                className={classes.tab} 
+                                component={Link} to="/about" />
+                            <Tab 
+                                label="Contact Us" 
+                                className={classes.tab} 
+                                component={Link} to="/contact" />
+                        </Tabs>
+                        <Button 
+                            variant="contained" 
+                            color="secondary" 
+                            className={classes.button}
+                            component={Link} to="/estimate">
+                            Free Estimate
+                        </Button>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
