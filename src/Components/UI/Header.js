ca63e67ac0ddce3680c84eb3daba5c6f752aea68
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-10-22 20:02:34 -0600

    menu dropdown finished

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -54,6 +54,18 @@ function ElevationScroll(props) {
           marginRight: "50px",
           height: "45px",
       },
+      menu: {
+          backgroundColor: theme.palette.common.blue,
+          color: "white",
+          borderRadius: "0px"
+      },
+      menuItem: {
+          ...theme.typography.tab,
+          opacity: 0.7,
+          "&:hover": {
+              opacity: 1
+          }
+      }
       
   }))
 
@@ -62,6 +74,7 @@ export default function Header(props){
     const [value, setValue] = useState(0)
     const [anchorEl, setAnchorEl] = useState(null)
     const [open, setOpen] = useState(false)
+    const [selectedIndex, setSelectedIndex] = useState(0)
 
     const handleChange = (e, value) => {
         setValue(value)
@@ -70,24 +83,89 @@ export default function Header(props){
         setAnchorEl(e.currentTarget)
         setOpen(true)
     }
+    const handleMenuItemClick = (e, i) =>{
+        setAnchorEl(null)
+        setOpen(false)
+        setSelectedIndex(i)
+    }
     const handleClose = (e) => {
         setAnchorEl(null)
         setOpen(false)
     }
+    const menuOptions = [
+    {name: "Services", link: "/services"},
+    {name: "Custom Software", link: "/customsoftware"},
+    {name: "Mobile App Development", link: "/mobileapps"}, 
+    {name: "Website Development", link: "/websites"}
+]
 
     useEffect(() => {
-        if(window.location.pathname === "/" && value !== 0) {
-            setValue(0)
-        } else if (window.location.pathname === "/services" && value !== 1) {
-            setValue(1)
-        } else if (window.location.pathname === "/revolution" && value !== 2) {
-            setValue(2)
-        } else if (window.location.pathname === "/about" && value !== 3) {
-            setValue(3)
-        } else if (window.location.pathname === "/contact" && value !== 4) {
-            setValue(4)
-        } else if (window.location.pathname === "/estimate" && value !== 5) {
-            setValue(5)
+        // if(window.location.pathname === "/" && value !== 0) {
+        //     setValue(0)
+        // } else if (window.location.pathname === "/services" && value !== 1) {
+        //     setValue(1)
+        // } else if (window.location.pathname === "/revolution" && value !== 2) {
+        //     setValue(2)
+        // } else if (window.location.pathname === "/about" && value !== 3) {
+        //     setValue(3)
+        // } else if (window.location.pathname === "/contact" && value !== 4) {
+        //     setValue(4)
+        // } else if (window.location.pathname === "/estimate" && value !== 5) {
+        //     setValue(5)
+        // }
+
+        switch(window.location.pathname){
+            case "/":
+                if(value !== 0){
+                    setValue(0)
+                }
+                break
+            case "/services":
+                if(value !== 1){
+                    setValue(1)
+                    setSelectedIndex(0)
+                }
+                break
+            case "/customsoftware":
+                if(value !== 1){
+                    setValue(1)
+                    setSelectedIndex(1)
+                }
+                break
+            case "/mobileapps":
+                if(value !== 1){
+                    setValue(1)
+                    setSelectedIndex(2)
+                }
+                break
+            case "/websites":
+                if(value !== 1){
+                    setValue(1)
+                    setSelectedIndex(3)
+                }
+                break
+            case "/revolution":
+                if(value !== 2){
+                    setValue(2)
+                }
+                break
+            case "/about":
+                if(value !== 3){
+                    setValue(3)
+                }
+                break
+            case "/contact":
+                if(value !== 4){
+                    setValue(4)
+                }
+                break
+            case "/estimate":
+                if(value !== 5){
+                    setValue(5)
+                }
+                break
+            default: 
+                break
         }
     }, [value])
 
@@ -109,7 +187,7 @@ export default function Header(props){
                             value={value} 
                             className={classes.tabContainer} 
                             onChange={handleChange}
-                            indicatorColor="secondary">
+                            indicatorColor="primary">
                             <Tab 
                                 label="Home" 
                                 className={classes.tab} 
@@ -146,23 +224,20 @@ export default function Header(props){
                             anchorEl={anchorEl} 
                             open={open}
                             onclose={handleClose}
-                            MenuListProps={{onMouseLeave: handleClose}} >
-                            <MenuItem 
-                                onClick={() => {handleClose(); setValue(1)}} 
-                                component={Link} to="/services"> 
-                            Services</MenuItem>
-                            <MenuItem 
-                                onClick={() => {handleClose(); setValue(1)}} 
-                                component={Link} to="/customsoftware"> 
-                            Custom Software</MenuItem>
-                            <MenuItem 
-                                onClick={() => {handleClose(); setValue(1)}} 
-                                component={Link} to="/mobileapps" > 
-                            Mobile App Development</MenuItem>
-                            <MenuItem 
-                                onClick={() => {handleClose(); setValue(1)}} 
-                                component={Link} to="/websites"> 
-                            Website Development</MenuItem>
+                            classes={{paper: classes.menu}}
+                            MenuListProps={{onMouseLeave: handleClose}}
+                            elevation={0} >
+                            {menuOptions.map((option, i) => (
+                                <MenuItem component={Link} to={option.link}
+                                key={option}
+                                classes={{root: classes.menuItem}}
+                                onClick={(event) => {
+                                    {handleMenuItemClick(event, i); setValue(1); handleClose()}
+                                }}
+                                selected={i === selectedIndex && value === 1}>
+                                    {option.name}
+                                </MenuItem>
+                            ))}
                         </Menu>
                     </Toolbar>
                 </AppBar>
